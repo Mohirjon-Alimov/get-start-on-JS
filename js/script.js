94458b1d7ded8d74c5a1599b42dae46e8e12fde8
Mohirjon <mohir7209@gmail.com>
2022-04-07 13:03:32 +0500

    add some canges

--- js/script.js
+++ js/script.js
@@ -8,8 +8,16 @@ var whereYouAreWork = prompt("qayeda ishlaysiz?");
 
 var yourJob = prompt("qaysi lavozimda ishlaysiz");
 
+//====================== console-1=================
 
-console.log("ismingiz:" + questionName + " ishingiz: " +  whereYouAreWork + " lavozimingiz: " + yourJob);
+console.log("ismingiz:" + questionName + " ish joyngiz: " +  whereYouAreWork + " lavozimingiz: " + yourJob);
+
+//====================== console-2=================
+
+console.log(`ismingiz: ${questionName}
+ish joyingiz: ${whereYouAreWork}
+lavozimingiz: ${yourJob} `)
+
 
 
 // var firstName = "bitasi";
