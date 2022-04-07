00edb791c2de9d245bceccfcb6c5e03485d6751a
Mohirjon <mohir7209@gmail.com>
2022-04-07 12:32:46 +0500

    git init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+alert("salom, xush kelibsiz");
+
+var questionName = prompt("ismingiz nma?");
+
+
+var whereYouAreWork = prompt("qayeda ishlaysiz?");
+
+
+var yourJob = prompt("qaysi lavozimda ishlaysiz");
+
+
+console.log("ismingiz:" + questionName + " ishingiz: " +  whereYouAreWork + " lavozimingiz: " + yourJob);
+
+
+// var firstName = "bitasi";
+// console.log("salom " + firstName);
+// var age_of_student = 19;
+// console.log("salom " + firstName + " " + "yosh: " + age_of_student);
